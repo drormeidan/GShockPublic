9aa75d976e6d7088fe557df2a4f0212b8cebca48
Dror Meidan <meidan.dror@gmail.com>
2021-01-20 18:57:58 +0200

    display

---
+++ GShock.API/GShock.Core/ResponseTypes/Displays/DisplayObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GShock.Core.ResponseTypes.Displays
+{
+    public class DisplayObject
+    {
+        public DisplayObject(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ GShock.API/GShock.Core/ResponseTypes/Displays/StringDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GShock.Core.ResponseTypes.Displays
+{
+    public class StringDisplay : DisplayObject
+    {
+        public string Value { get; set; }
+        public StringDisplay(string value): base("string")
+        {
+            Value = value;
+        }
+    }
+}
